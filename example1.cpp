6d5f535a68977ba419894ce00b1182d7c13c5b4f
Hubert Droździak <hubertdrozdziak@gmail.com>
2022-03-29 22:39:14 +0200

    example1 fixed

--- example1.cpp
+++ example1.cpp
@@ -5,6 +5,9 @@ int main()
     int* num = new int(10);
     std::cout << "Hello World!" << std::endl
               << "Created new int: " << *num << std::endl;
+
+    delete num;
+
     return 0;
 }
 
